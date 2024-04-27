0368d6b1169ade4cef6253de6696590023be9ed3
zhaqyy <zaqrashyy7@gmail.com>
2024-04-27 23:33:18 +0100

    scene display fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
 import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Box, OrbitControls } from "@react-three/drei";
+import LineHero from "./LineHero";
 
-const Scene = () => {
-  const boxRef = useRef();
-  useFrame((state, delta) => {
-    boxRef.current.rotation.y += 0.02;
-  });
-
-  return (
-    <>
-      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
-        <meshNormalMaterial />
-      </Box>
-      <ambientLight />
-    </>
-  );
-};
 
 const App = () => {
   return (
-    <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
-      <OrbitControls />
-      <Scene />
-    </Canvas>
+   
+      <LineHero/>
+      
   );
 };
 
